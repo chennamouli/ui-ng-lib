967f6eb2bc7dbd01813fd4a2eeb631afd6190cdf
Chandramouli Gajam <chandramouli.gajam@trinet.com>
2020-12-15 13:42:41 -0600

    Added ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(public translate: TranslateService) {
+    translate.setDefaultLang('en-US');
+    translate.use('en-US');
+  }
   title = 'ui-ng-lib';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataEntryComponent } from './feature/components/data-entry/data-entry.component';
 import { HomeComponent } from './feature/components/home/home.component';
+
+export function createTranslateLoader(http: HttpClient) {
+  // cache-buster, cb=time query paramer prevents from caching this file at the client side.
+  return new TranslateHttpLoader(
+    http,
+    'assets/i18n/',
+    '.json?cb=' + new Date().getTime()
+  );
+}
+
 @NgModule({
   declarations: [AppComponent, DataEntryComponent, HomeComponent],
   imports: [
@@ -17,6 +29,13 @@ import { HomeComponent } from './feature/components/home/home.component';
     FormsModule,
     ReactiveFormsModule,
     NgbModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
     SharedModule,
   ],
   providers: [],
